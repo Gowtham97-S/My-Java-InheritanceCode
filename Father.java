ac01488644eacb693aae058e05d61b30b8ab3aee
Gowtham <121371102+Gowtham97-S@users.noreply.github.com>
2023-01-07 20:44:07 +0530

    Updated

--- Father.java
+++ Father.java
@@ -4,15 +4,14 @@ public class Father extends GrandPa{
 	
 	  int b = 50;
 	public void father01(){
-		System.out.println("Father Class");
+	System.out.println("Father Class");
 	}
-		public static void main (String[]args) {
-			Father multi = new Father();
-			multi.family();
-			System.out.println("GrandPa age is " + multi.p);
-			
-			multi.father01();
-			System.out.println("Father age is " + multi.b);
+	public static void main (String[]args) {
+	    Father multi = new Father();
+	    multi.family();
+	    System.out.println("GrandPa age is " + multi.p);
+	    multi.father01();
+	    System.out.println("Father age is " + multi.b);
 		}
 	
 }
